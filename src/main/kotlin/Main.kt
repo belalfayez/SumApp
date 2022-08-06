86e1676c894210f7e3223c2637b6a5fb0bcbbd5c
Belal Fayez <37405227+bilalfayez@users.noreply.github.com>
2022-08-06 06:38:24 +0200

    Welcome Msg

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
+import java.util.Scanner
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    //Welcome To the project
+
+    //step one take an input from user
+    val scr = Scanner(System.`in`)
+    val name = scr.nextLine()
+    println("Welcome " + name + "to the project" )
+
+
 }
